0e6fa4a08449e983d61eeef0944261ada952c25c
liusong <liusong@glaway.com>
2020-01-06 11:19:26 +0800

    b站视频和vip音乐，其中B站视频下载用到了ffmpy3第三方库以及ffmpeg程序

--- scrampy/vip_music.py
+++ scrampy/vip_music.py
@@ -6,9 +6,12 @@ import json
 url = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token='
 headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+#'params': 'b9v9eZseYhH2aUzxthwUFOLYIFZCw/D6wb5A050irvQAcONqOmvAQzMlqHwdy/gwzmiJCSHnpnIEuYmJ37CJmNX+PB+rZc0V+wqhc/5VNoj63L61xaYHKCo42ftXrFRqgXYGr72wl+SrlkdIbfOi496M2X8V8AFLiV6XisCKwtVuswz7U21kPHTiqYgAp3eILltZBZBvURryG8woRnOhAcZxsVI+SeRKxFirFpywJdmsRujQd36EKTbrccLxFDcWTwhKMDXqfcT4sjNKcSzcKQtk/4RSJmJ2RctBYY7VOJFCAYxZPcjTsNkh8AaHmSOb',
+#'encSecKey': '503473307c750c6c278c5a4ad368156038171060ea092e2fb63e864761feedfcb989d4553e65abc185ec6b87cfd8e9f79db44037b63941e2f31e2b31fed4d326a1e64a10eaf1e8aee6704984a0ef6a5e08130a8313814b9d8176a86f8b39b10a7e49b3d0ea5d482aa38ba60d4140344861f53b3cb0bada8fa88cfee7c2cefda3'}
+
 data = {
-    'params': 'idokQyeL84XyJfl4Lej1S/nFjDQqQCkfuEg1FgF+ktqUGAUzhdKOLGKbonS/YaHEl2B8gxKCwjq0klneLEwWDzGuoDxLcrUhlgYLIYE1ot7aO/A94sN3PO35e2jMm6C9vReYwfESX2nI18hN4ZlIg8JL9p3lPAOMJhB11+tXbdGVE1jjRi6H5VLpPpItm0ybMvp7cDGOxmXMYXcPDEOEsjl6U5Oce2OpreWK9v7bi5VAqSkoZVTsr/7rToAxWEYwhf5ikafTg4hP82xqYDhEvv2YnlFjW9Rrj1WxeVtfjCwmL4f1TAgh4P3j1lmEdkA7',
-    'encSecKey': '4ce11203082e9afafa0200d00d3806be222167e2edf57236f101e5a2b9914883d4bdb227c8675ce8d084f56a01c2d0558e0afbb86ebbcb07f16a9552af34de29413c1785921497a3d93272c1477c65b3dc2550c2659e36fab608f48b7ac3712176b99f9f3d016aa46b75486688dd5da51df170da40389f004f203683920661a0'}
+    'params': 'gaZAfJbEYDZGAwHSjq/g5yv4C2k22TclVDDyMuU4haIWJtyHT4bFLISx9SLpJJK9JX5bpeJIxE6wfg1z1fWNHTl2DXbSQVM9IJeSyJVClMo=',
+    'encSecKey': '59a4424dc6f1ee91f075881fb11a60b196855217f9c08ee4a47880a74ba50dea38265d89711875ae2ebf0771b97c044c1f24ff1d063edfd8562a1d0f5f25c1ae63e17cbeebbb68bb2c712a02661c20bdb49f3f3e11fc355172f039ac3cede1a1b920bd0cad79f705cd37eb7a0b8cc2e45ccedeb0b1dfbc56d7b04f4e51dee99b'}
 def get_music():
     r = requests.post(url,headers=headers,data=data)
     rjson = r.json()
@@ -32,4 +35,5 @@ def download_music():
             f.write(r)
 
 
-download_music()
+if __name__ == '__main__':
+    download_music()
