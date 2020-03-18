97ad19be3a31b227c79fb9c3a978b2963e63932b
liusong <liusong@glaway.com>
2020-03-18 17:44:40 +0800

    [description]:review python base

--- my_review.py
+++ my_review.py
@@ -53,8 +53,59 @@ def some_origin_fun():
     #iter对所有可迭代的对象都适用
     for i in iter(y):
         print(i)
+    #dict use, dict是关键字
+    dicts = dict(one='wes',second='hello',third=3)
+    #这是一个map
+    dicts1 =dict((['x',1],['y',2]))
+    print(dicts,dicts1)
+    for key in dicts.keys():
+        value = dicts[key]
+def format_print():
+    # %f 浮点数 %e 科学计数法 %d 有符号十进制 %o 八进制 %x 16进制 %s string %u 无符号十进制
+    print("we are %d" %18)
+    print("we are %x" %14)
+    print("we are %e" %123)
+    #原始字符串，r后面的字符串保持原样输出
+    print(r"\n")
+
+def use_for():
+    #common use
+    a = [1,2,5,4]
+    for i in a:
+        print(i)
+    # for循环会在循环可迭代对象时，自动调用其next()方法，比java的方便 while i.hasNext(),i是迭代器
+    b = iter(a)
+    for i in b:
+        print(i)
+    # 复杂列表推导式
+    c = [x*3 for x in range(10) if x <3]
+    for i in c:
+        print(i)
+
+def file_rw():
+    # access_mode: a append ;w write ;+ write and read ;b byte; U 支持通用换行符
+    # f = open("test/trywrite_file.txt",'a')
+    # f.write('\nappend something')
+    # f.close(
+    #逐行读取
+    b = open('test/trywrite_file.txt','r')
+    for line in b:
+        print(line)
+    b.close()
+    print('--------')
+    #当某个文件被读取完之后，其指针已经到末尾因此想再次获取该文件只有重新打开，和tuple的generator一样
+    #返回的list
+    m = open('test/trywrite_file.txt','r')
+    print(m.readlines())
+    m.close()
+
+
+
 if __name__ == "__main__":
     # fun_type()
     # seq_slice()
     # fun_is()
-    some_origin_fun()
+    # some_origin_fun()
+    # format_print()
+    # use_for()
+    file_rw()
